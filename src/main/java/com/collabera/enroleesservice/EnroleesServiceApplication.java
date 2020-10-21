9fbab98a875ed5d481c5c3b5af5baae8e2b1e0a6
Ildephonse BIKINO <ildephonsebikino@Ildephonses-MacBook-Pro.local>
2020-10-21 10:29:19 -0400

    First commit...

---
+++ src/main/java/com/collabera/enroleesservice/EnroleesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.collabera.enroleesservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnroleesServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnroleesServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/collabera/enroleesservice/EnroleesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collabera.enroleesservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnroleesServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
